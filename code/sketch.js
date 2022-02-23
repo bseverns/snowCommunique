2fe745b378c767534ecb784be6b6e3b750b4aac2
Ben Severns <severns3@gmail.com>
2022-02-23 17:50:45 -0600

    shifting DOM burden to .js

--- code/sketch.js
+++ code/sketch.js
@@ -5,9 +5,25 @@ let down = 1;
 
 let dots = [];
 
+let cnv, im1, im2, im3, im4, im5, im6, im7, im8, im9, im10, im11ß;
+
+function preload() {
+  im1 = createImg("assets/season.jpg","happy holidays");
+  im2 = createImg("assets/wed.gif","wedding bells");
+  im3 = createImg("assets/ben1.jpg","ben's headshot");
+  im4 = createImg("assets/leen1.jpg","colleen's face at home");
+  im5 = createImg("assets/SLKM.jpg","us doing our thing");
+  im6 = creatImg("assets/cats.jpg", "the cats!");
+  im7 = createImg("assets/benart1.png","things that ben does");
+  im8 = createImg("assets/benart2.png","things ben has done");
+  im9 = createImg("assets/benart3.png","things that ben does");
+  im10 = createImg("assets/benart4.png","things that ben does");
+  im11 = createImg("assets/leencat1.jpg","leen and the rocket");
+}
+
 // Will run once when the sketch is opened
 function setup() {
-  var cnv = createCanvas(windowWidth, 200);
+  cnv = createCanvas(windowWidth, 200);
   cnv.parent('sketch-holder');
   noStroke();
 
